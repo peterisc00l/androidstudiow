ebcb4498c9ac5e6d102e3780c06f011a3a417d40
peterisc00l <augustambaekwe@gmail.com>
2025-09-19 18:59:28 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,21 @@
-from flask import*
-from flask_mail import*
-from random import*
+from flask import Flask, render_template
+from forms import ContactForm
 
 app = Flask(__name__)
-
-app.config["MAIL_SERVER"] = 'smtp.gmail.com'
-app.config["MAIL_PORT"] = 465
-app.config["MAIL_USERNAME"] = 'pete6767676@gmail.com'
-app.config["MAIL_PASSWORD"] = '6666677777778910'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-
-otp = randint(000000,999999)
-
-@app.route('/verify',methods = ["POST"])
-def verify():
-    email = request.form["email"]
-    msg = Message('OTP',sender = 'peterand156967@gmail.com', recipients = [email])
-    msg.body = str(otp)
-    Mail.send(msg)
-    return render_template('page.html')
-
-@app.route('/vaildate',method = ['POST'])
-def vaildate():
-    user_otp = request.form["otp"]
-    if otp == int(user_otp):
-        return "<h3>email verification successful</h3>"
-    else:
-        return "<h3>Verification failed otp does not match</h3>"
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+app.secret_key = 'development key'
 
 
+
+
+@app.route('/', methods=['GET','POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        return render_template('index.html')
+    return render_template('index.html', form-form)
+
+@app.route('/success', methods=['GET', 'POST'])
+def success():
+    return render_template("page.html")
 
 app.run(host='0.0.0.0', port=8080)
